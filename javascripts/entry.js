a884d49b58abc16d5256270f06c137575b2f695f
Ted Shaffer <ted@brightsign.biz>
2016-05-14 17:05:38 -0700

    initial commit

--- javascripts/entry.js
+++ javascripts/entry.js
@@ -2,17 +2,26 @@ require('../less/main.less');
 
 'use strict';
 
-var React = require('react');
-var ReactDOM = require('react-dom');
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
-var Entry = React.createClass({
-  render: function(){
-    return (
-      <div className="myDiv">
-        Hello Electron!
-      </div>
-    )
-  }
-});
+// var App = React.createClass({
+  // render: function(){
+  //   return (
+  //     <div className="myDiv">
+  //       Hello Electron!
+  //     </div>
+  //   )
+  // }
+const App = () => {
+  return (
+    <div className="myDiv">
+      I am the search bar
+    </div>
+  );
+}
 
-ReactDOM.render(<Entry />, document.getElementById('content'));
+ReactDOM.render(<App />, document.getElementById('content'));
