243c292c271777e23e90345283b08b53d63851b9
Ted Shaffer <ted@brightsign.biz>
2016-05-15 05:56:04 -0700

    remove commented out code

--- scripts/index.js
+++ scripts/index.js
@@ -13,33 +13,6 @@ import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
-
-// var App = React.createClass({
-  // render: function(){
-  //   return (
-  //     <div className="myDiv">
-  //       Hello Electron!
-  //     </div>
-  //   )
-  // }
-// const App = () => {
-//   return (
-//     <div className="myDiv">
-//         <App />
-//     </div>
-//   );
-// }
-
-// ReactDOM.render(
-//     <App />,
-//     document.getElementById('content'));
-
-// ReactDOM.render(
-//     <Provider store={createStoreWithMiddleware(reducers)}>
-//         <App />
-//     </Provider>
-//     , document.querySelector('.content'));
-
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
         <App />
