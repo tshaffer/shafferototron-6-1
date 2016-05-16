a7f9ef0e9542ac551dedafbfa50848beb0171fa1
Ted Shaffer <ted@brightsign.biz>
2016-05-16 16:26:20 -0700

    moved app menu to photos

--- scripts/containers/photos.js
+++ scripts/containers/photos.js
@@ -10,6 +10,9 @@ import { bindActionCreators } from 'redux';
 import PhotoGrid from '../containers/photo-grid';
 import PhotoDetail from '../containers/photo_detail';
 
+const {remote} = require('electron');
+const {Menu, MenuItem} = remote;
+
 class Photos extends Component {
 
     constructor(props) {
@@ -38,6 +41,78 @@ class Photos extends Component {
             height: window.innerHeight - 100
         };
         this.setState({divStyle: divStyle});
+
+        const menuTemplate = [
+            {
+                label: 'File',
+                submenu: [
+                    {
+                        label: 'Add Folder'
+                    }
+                ]
+            }
+        ];
+
+        if (process.platform === 'darwin') {
+            const name = remote.app.getName();
+            menuTemplate.unshift({
+                label: name,
+                submenu: [
+                    {
+                        label: 'About ' + name,
+                        role: 'about'
+                    },
+                    {
+                        type: 'separator'
+                    },
+                    {
+                        label: 'Services',
+                        role: 'services',
+                        submenu: []
+                    },
+                    {
+                        type: 'separator'
+                    },
+                    {
+                        label: 'Hide ' + name,
+                        accelerator: 'Command+H',
+                        role: 'hide'
+                    },
+                    {
+                        label: 'Hide Others',
+                        accelerator: 'Command+Alt+H',
+                        role: 'hideothers'
+                    },
+                    {
+                        label: 'Show All',
+                        role: 'unhide'
+                    },
+                    {
+                        type: 'separator'
+                    },
+                    {
+                        label: 'Quit',
+                        accelerator: 'Command+Q',
+                        click: function() {
+                            remote.app.quit();
+                        }
+                    },
+                ]
+            });
+            // Window menu.
+            // menuTemplate[3].submenu.push(
+            //     {
+            //         type: 'separator'
+            //     },
+            //     {
+            //         label: 'Bring All to Front',
+            //         role: 'front'
+            //     }
+            // );
+        }
+
+        const menu = Menu.buildFromTemplate(menuTemplate);
+        Menu.setApplicationMenu(menu);
 
     }
 
