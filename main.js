655100acf4669c5fb2abc75d8e48302e65b35565
Ted Shaffer <ted@brightsign.biz>
2016-05-29 13:51:39 -0700

    upgrade properly to electron version 1.2.0

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const path = require('path')
-const glob = require('glob')
-const electron = require('electron')
 
-// var app = require('app');
-const BrowserWindow = require('browser-window');
-const app = electron.app
+const electron = require('electron');
 
-require('crash-reporter').start();
+// Module to control application life.
+const {app} = electron;
+
+// Module to create native browser window.
+const {BrowserWindow} = electron;
 
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin') {
@@ -14,21 +14,47 @@ app.on('window-all-closed', function() {
   }
 });
 
-app.on('ready', function() {
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
 
-  // local main process files
-  var files = glob.sync(path.join(__dirname, 'mainProcess/*.js'))
-  files.forEach(function (file) {
-    require(file)
-  })
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({width: 1400, height: 800});
 
-  mainWindow = new BrowserWindow({width: 1360, height: 800});
+  // and load the index.html of the app.
+  win.loadURL(`file://${__dirname}/index.html`);
 
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  // Open the DevTools.
+  win.webContents.openDevTools();
 
-  mainWindow.openDevTools();
-
-  mainWindow.on('closed', function() {
-    mainWindow = null;
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
   });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  }
 });
